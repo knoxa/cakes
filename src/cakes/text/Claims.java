3a1a51a9575510815e6c7476842282609ea4e58b
knoxa <swaffhamtown@outlook.com>
2025-10-20 15:20:48 +0100

    simplifying various parsing methods

--- src/cakes/text/Claims.java
+++ src/cakes/text/Claims.java
@@ -20,7 +20,10 @@ public class Claims {
 	
 	/*
 	 *  Exploit the controlled vocabulary of claims to do some light-weight NLP.
+	 *  There is no formal CE - just try and make the best of simple sentences...
 	 */
+	
+	private static final Pattern be = Pattern.compile("\\s+(is|are)\\s+?");
 
 	public static Map<String, Set<String>> equivalentNames(Collection<String> claims) {
 		
@@ -47,31 +50,82 @@ public class Claims {
 	}
 	
 
-	public static Map<String, Set<String>> placeInRegion(Collection<String> claims) {
+	public static Map<String, Set<String>> placeIn(Collection<String> phrases) {
 
 		/*
-		 * Reads facts expressed in "controlled" English (CE). No formal rules for expression of CE - just simple sentence that should be easy
-		 * to parse given some context.
+		 * Parse phrases that link places to a containing region.
+		 * Return phrase |-> region
 		 * 
-		 * Parse sentences that link a smaller place to a containing region. Creates a context where the objects are regions and the attributes
-		 * are places they contain.
+		 * Matches: "a place in ...", "places in ..." and "in ..."
+		 * 
+		 */
+    	
+		Map<String, Set<String>> results = new HashMap<String, Set<String>>();
+		Pattern pattern = Pattern.compile("(?i)^((a\\s+place|places)\\s+)?in\\s+");
+
+		for ( String phrase: phrases ) {
+			
+			Matcher matcher = pattern.matcher(phrase);
+			
+			if (matcher.find() ) {
+				
+				Maps.addMapValue(results, phrase, phrase.substring(matcher.end()).trim());
+			}
+		}
+		
+		return results;
+	}
+	
+
+	public static Map<String, Set<String>> thingsThatAre(Collection<String> claims) {
+
+		/*
+		 * Parse sentences that contain "is a" or "are" and assume that one or more entities precede it.
+		 * Return claim |-> set of entities
 		 * 
 		 * Claims should be single sentences.
 		 * 
 		 */
     	
 		Map<String, Set<String>> results = new HashMap<String, Set<String>>();
-		Pattern pattern = Pattern.compile("\\s+(is a place in|are places in)\\s+([^\\.]*)\\.");
 
 		for ( String claim: claims ) {
 			
-			Matcher matcher = pattern.matcher(claim);
+			Matcher matcher = be.matcher(claim);
 			
 			if (matcher.find() ) {
 				
-				Set<String> placeNames = new HashSet<String>();
-				placeNames.addAll(getEntitiesFromListPhrase(claim.substring(0, matcher.start())));					
-				Maps.addMapValues(results, matcher.group(2), placeNames);
+				Set<String> entities = new HashSet<String>();
+				entities.addAll(getEntitiesFromListPhrase(claim.substring(0, matcher.start())));					
+				Maps.addMapValues(results,claim, entities);
+			}
+		}
+		
+		return results;
+	}
+	
+
+	public static Map<String, Set<String>> whatThingsAre(Collection<String> claims) {
+
+		/*
+		 * Parse sentences that contain "is a" or "are" and assume that what follows it is a description.
+		 * Return claim |-> description
+		 * 
+		 * Claims should be single sentences.
+		 * 
+		 */
+    	
+		Map<String, Set<String>> results = new HashMap<String, Set<String>>();
+
+		for ( String claim: claims ) {
+			
+			Matcher matcher = be.matcher(claim);
+			
+			if (matcher.find() ) {
+				
+				Set<String> description = new HashSet<String>();
+				description.add(claim.substring(matcher.end()).replaceFirst(".$", ""));					
+				Maps.addMapValues(results,claim, description);
 			}
 		}
 		
@@ -84,9 +138,11 @@ public class Claims {
 		/*
 		 * Split a text list into the listed items.
 		 */
-		
+
+		//ignore leading determiner
+		String list = text.replaceAll("(?i)^(the|an|a|there|they)\\b", "").trim();
 		Set<String> results = new HashSet<String>();
-		String[] items = text.split(",\\s*|\\sand\\s");
+		String[] items = list.split(",\\s*(and\\s+)?|\\s+and\\s+");
 		results.addAll(Arrays.asList(items));
 		
 		return results;
