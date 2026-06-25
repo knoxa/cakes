033e15ba5c7a58105cd632457de2b39b56e0f5c4
knoxa <swaffhamtown@outlook.com>
2026-06-25 20:57:11 +0100

    added getDictionaryMap()

--- src/cakes/text/SimpleTextDocument.java
+++ src/cakes/text/SimpleTextDocument.java
@@ -1,10 +1,15 @@
 package cakes.text;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class SimpleTextDocument implements TextDocument {
 
 	private String text = null;
 	private String uri = null;
-	
+	private Map<String, Set<String>> dictionaryMap = null;
+
 	@Override
 	public String getTextContent() {
 		return text;
@@ -17,9 +22,19 @@ public class SimpleTextDocument implements TextDocument {
 
 	public SimpleTextDocument(String text, String uri) {
 		
-		this.text = text;
-		this.uri = uri;
+		this(text, uri, new HashMap<String, Set<String>>());
 	}
 
+	public SimpleTextDocument(String text, String uri, Map<String, Set<String>> dictionaryMap) {
+		
+		this.text = text;
+		this.uri = uri;
+		this.dictionaryMap = dictionaryMap;
+	}
+	
+	@Override
+	public Map<String, Set<String>> getDictionaryMap() {
+		return dictionaryMap;
+	}
 	
 }
--- src/cakes/text/TextDocument.java
+++ src/cakes/text/TextDocument.java
@@ -1,7 +1,11 @@
 package cakes.text;
+import java.util.Map;
+import java.util.Set;
 
 public interface TextDocument {
 
 	public String getTextContent();
 	public String getUri();
+	public Map<String, Set<String>> getDictionaryMap();
+
 }
