9d9ec076f3619c06966b87698d1f1331cd0766aa
knoxa <swaffhamtown@outlook.com>
2026-07-14 16:11:50 +0100

    normalize tokens

--- src/cakes/nlp/dictionary/DefaultTokenizer.java
+++ src/cakes/nlp/dictionary/DefaultTokenizer.java
@@ -3,6 +3,7 @@ package cakes.nlp.dictionary;
 import java.text.BreakIterator;
 //import java.text.Normalizer.Form;
 //import org.apache.commons.lang3.StringUtils;
+import java.text.Normalizer.Form;
 
 import cakes.nlp.core.Span;
 
@@ -10,9 +11,9 @@ public class DefaultTokenizer implements Tokenizer {
 
 	public Span[] tokenize(String text) {
 		
-    	//String normal = java.text.Normalizer.normalize(text, Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+    	String normal = java.text.Normalizer.normalize(text, Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
     	//String normal = StringUtils.stripAccents(text);
-		String normal = text;
+		//String normal = text;
 		return cakes.nlp.parse.Tokenizer.split(normal, BreakIterator.getWordInstance());
 	}
 
